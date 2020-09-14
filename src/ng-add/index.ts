67f88dd2a2dfd0ba2794f5c1dbfffed4e95fd7ef
Sanjiv Kumar <sanjiv_kumar@vrtx.com>
2020-09-14 00:34:53 -0400

    Rename to ng-add

--- src/ng-add/index.ts
+++ src/ng-add/index.ts
@@ -1,9 +1,8 @@
 import { Rule, SchematicContext, Tree } from '@angular-devkit/schematics';
 
-
-// You don't have to export the function as default. You can also have more than one rule factory
-// per file.
-export function angularTailwindcssSchematics(_options: any): Rule {
+/** Rule factory: returns a rule (function) */
+export default function (_options: any): Rule {
+  // this is a rule. It takes a  `tree` and returns another `tree`.
   return (tree: Tree, _context: SchematicContext) => {
     return tree;
   };
