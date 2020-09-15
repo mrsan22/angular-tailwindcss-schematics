8766553c071e6ba8eb08ee3cae1a1f0813de5450
Sanjiv Kumar <sanjiv_kumar@vrtx.com>
2020-09-15 01:12:49 -0400

    npm link is working from sample app

--- src/angular-tailwindcss-schematics/index.ts
+++ src/angular-tailwindcss-schematics/index.ts
@@ -4,6 +4,8 @@ import { Rule, SchematicContext, Tree } from '@angular-devkit/schematics';
 export default function (_options: any): Rule {
   // this is a rule. It takes a  `tree` and returns another `tree`.
   return (tree: Tree, _context: SchematicContext) => {
+    console.log('schematic works');
+    tree.create(_options.name || 'hello', 'world');
     return tree;
   };
 }
