288eb84932b57d5507c8028b8f620c9e563832aa
Sanjiv Kumar <sanjiv_kumar@vrtx.com>
2020-09-16 18:50:33 -0400

    functionality to add config files for tailwindcss

--- src/ng-add/index.ts
+++ src/ng-add/index.ts
@@ -1,4 +1,17 @@
-import { chain, Rule, SchematicContext, SchematicsException, Tree } from '@angular-devkit/schematics';
+import { normalize, strings } from '@angular-devkit/core';
+import { workspace } from '@angular-devkit/core/src/experimental';
+import {
+  apply,
+  chain,
+  mergeWith,
+  move,
+  Rule,
+  SchematicContext,
+  SchematicsException,
+  template,
+  Tree,
+  url,
+} from '@angular-devkit/schematics';
 import { addPackageJsonDependency, NodeDependency, NodeDependencyType } from '@schematics/angular/utility/dependencies';
 import { Schema } from './schema';
 import { tailwindcssDependencies } from './utils';
@@ -13,41 +26,23 @@ export default function (options: Schema): Rule {
     if (!workspaceConfigBuffer) {
       throw new SchematicsException('Could not find an Angular workspace configuration');
     }
-    // // parse config only when not null
-    // const workspaceConfig: workspace.WorkspaceSchema = JSON.parse(workspaceConfigBuffer.toString());
+    // parse config only when not null
+    const workspaceConfig: workspace.WorkspaceSchema = JSON.parse(workspaceConfigBuffer.toString());
     // // if project is not passed (--project), use default project name
-    // if (!options.project && workspaceConfig.defaultProject) {
-    //   options.project = workspaceConfig.defaultProject;
-    // }
-    // const projectName = options.project as string;
-    // const projectName = options.project as string;
+    if (!options.project && workspaceConfig.defaultProject) {
+      options.project = workspaceConfig.defaultProject;
+    }
+    const projectName = options.project as string;
     // // select project from projects array in `angular.json` file
-    // const project: workspace.WorkspaceProject = workspaceConfig.projects[projectName];
-    // if (!project) {
-    //   throw new SchematicsException(`Project ${projectName} is not defined in this workspace.`);
-    // }
+    const project: workspace.WorkspaceProject = workspaceConfig.projects[projectName];
+    if (!project) {
+      throw new SchematicsException(`Project ${projectName} is not defined in this workspace.`);
+    }
     // const projectType = project.projectType === 'application' ? 'app' : 'lib';
     // Path to create the file
     // const defaultPath = `${project.sourceRoot}/${projectType}`;
-
-    // get hold of our templates files
-    // const sourceTemplates = url('./files');
-    /**
-     * `template` rule processes templates and returns rule.
-     * The template helpers like `dasherize` or `classify` are available because we’re spreading `strings` object into the `options` objectand then we’re passing into the template
-     */
-    // const sourceParamteterizedTemplates = apply(sourceTemplates, [
-    //   template({
-    //     ...options,
-    //     ...strings,
-    //     name: options.name,
-    //   }),
-    //   // move file to resolved path
-    //   move(normalize(defaultPath as string)),
-    // ]);
-    // mergeWith returns a Rule so it can be called with tree and context (not required though)
-    // merge our template into tree.
-    return chain([addDependencies(options)])(tree, context);
+    // compose all rules using chain Rule.
+    return chain([addDependencies(options), addTemplateFiles(options)])(tree, context);
   };
 }
 
@@ -79,6 +74,27 @@ function addDependencies(options: Schema): Rule {
 //     return tree;
 //   };
 // }
+
+function addTemplateFiles(options: Schema): Rule {
+  return (tree: Tree, context: SchematicContext) => {
+    // get hold of our templates files
+    const sourceTemplates = url('./files');
+    /**
+     * `template` rule processes templates and returns rule.
+     * The template helpers like `dasherize` or `classify` are available because we’re spreading `strings` object into the `options` objectand then we’re passing into the template
+     */
+    const sourceParamteterizedTemplates = apply(sourceTemplates, [
+      template({
+        ...options,
+        ...strings,
+        cssType: options.cssType,
+      }),
+      // move file to resolved path
+      move(normalize('./')),
+    ]);
+    return mergeWith(sourceParamteterizedTemplates)(tree, context);
+  };
+}
 
 function nodeDependencyFactory(dependencyName: string, options: Schema): NodeDependency {
   // default version : latest
